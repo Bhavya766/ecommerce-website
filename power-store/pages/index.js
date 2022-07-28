befa5f0282417a569a08985bc57f09309d4373e6
Bhavya766 <bhavyagada09876@gmail.com>
2022-07-28 14:06:36 +0530

    Functionality and layout
    Product renders, layout of most of pages and basic functionality completed.

--- power-store/pages/index.js
+++ power-store/pages/index.js
@@ -11,7 +11,12 @@ const Home = ({ products, bannerData }) => {
         <h2>Best selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
-      <div className="products-container"></div>
+      <div className="products-container">
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
+      </div>
+      <FooterBanner footerBanner={bannerData.length && bannerData[0]} />
     </>
   );
 };
